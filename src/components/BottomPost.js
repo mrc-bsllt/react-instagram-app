260a1c5744d2e74259e3961cf6b4a14650469cc1
MarcoB <mrc.bsllt@gmail.com>
2021-04-17 16:31:59 +0200

    comments
    static comments in progress

--- src/components/BottomPost.js
+++ src/components/BottomPost.js
@@ -1,5 +1,5 @@
 import "../css/BottomPost.css";
-
+import Comments from "./Comments";
 const BottomPost = () => {
     return (
         <div className="bottom_post">
@@ -7,6 +7,18 @@ const BottomPost = () => {
                 <img src="img/icons/heart.svg" alt="heart_icon"/>
                 <img src="img/icons/chat.svg" alt="chat_icon"/>
             </div>
+
+            <div className="feeds">
+                <img src="img/profilo.jpg" alt="first comment avatar"/>
+                <p>Piace a <strong>Username-primo-commento</strong> e <strong>altri 4</strong></p>
+            </div>
+
+            <div className="post_description">
+                <h4>Username-creator-post</h4>
+                <p>Descrizione del post...</p>
+            </div>
+
+            <Comments />
         </div>
     )
 }
---
+++ src/components/Comments.js
@@ -0,0 +1,11 @@
+import "../css/Comments.css";
+
+const Comments = () => {
+    return(
+        <div className="comments">
+            <p>Mostra tutti e 5 commenti</p>
+        </div>
+    )
+}
+
+export default Comments;
