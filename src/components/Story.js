7736edd15cee9bcd68b662a8057dccf8ddb26092
MarcoB <mrc.bsllt@gmail.com>
2021-04-27 16:53:49 +0200

    stories images
    added conditional rendering in place of dynamic classes

--- src/components/Story.js
+++ src/components/Story.js
@@ -14,21 +14,24 @@ const Story = ({ index, story }) => {
                 <h5>{ `${story.profile_name.substr(0, 8)}...` }</h5>
             </div>
 
-            <div className={ activeIndex === index ? 'show_story displa_block' : 'show_story display_none' }>
-                <div className="story_content">
-                    <div className="time_bar_container">
-                        <TimeBar />
+            {  activeIndex === index && (
+
+                <div className="show_story">
+                    <div className="story_content">
+                        <div className="time_bar_container">
+                            <TimeBar />
+                        </div>
+
+                        <div className="image_container">
+                        <img src={ story.story_image } alt="story_picture" />
+                        </div>
                     </div>
 
-                    <div className="image_container">
-                       <img src={ story.story_image } alt="story_picture" />
+                    <div className="exit_button">
+                        <i class="fas fa-times"></i>
                     </div>
                 </div>
-
-                <div className="exit_button">
-                    <i class="fas fa-times"></i>
-                </div>
-            </div>
+            )}
         </div>
     )
 }
